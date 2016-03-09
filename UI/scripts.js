8e37d909a6b9999bb158d937e258d1b57b61bc57
Maryia Hryhoryeva <mash-grig@mail.ru>
2016-03-09 23:29:52 +0300

    Updated

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,25 +1,40 @@
-var mesId =  0;
+var mesId = 0;
+var chosen = false;
+var chosenId;
 
 function send() {
     var message = document.createElement('div');
     message.classList.add('me');
-    mesId++;
-    message.id =  mesId;
-    message.setAttribute("onclick","active(mesId)");
-
+    message.id = 'm' + mesId;
+    message.setAttribute("onclick", "active(" + mesId + ")");
     var text = document.getElementById("message").value;
     message.appendChild(document.createTextNode(text));
     document.getElementById("chat").appendChild(message);
     document.getElementById("message").value = "";
-    }
-
-function active(id){
-    alert(id);
-    document.getElementById(id).classList.add('choose');
-
+    mesId++;
 }
 
-function deleted(id){
+function active(id) {
+    if (chosen && chosenId == id) {
+        document.getElementById('m' + chosenId).classList.remove('choose');
+        chosen = false;
+        document.getElementById('deletebutton').style.visibility = 'hidden';
+    } else if (!chosen) {
+        chosenId = id;
+        document.getElementById('m' + id).classList.add('choose');
+        chosen = true;
+        document.getElementById('deletebutton').style.visibility = 'visible';
+    }
+}
 
+function deleted() {
+    var to_delete = document.getElementById('m' + chosenId);
+    to_delete.classList.remove('me', 'choose');
+    to_delete.classList.add('delete');
+    to_delete.innerHTML = 'Deleted.';
+    to_delete.removeAttribute("onclick");
+    chosen = false;
+    chosenId = "";
+    document.getElementById('deletebutton').style.visibility = 'hidden';
 }
 
